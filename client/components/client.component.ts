bfd9c8940e21927ab400eb213d8614027d978c1a
I <bohao.cao@gmail.com>
2016-03-03 00:07:03 -0500

    3/2 at home

---
+++ client/components/client.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+	selector: 'migration-auto-runner',
+	templateUrl:'client/components/client.component.html'
+})
+
+
+export class ClientComponent{
+
+}
---
+++ client/main.ts
@@ -0,0 +1,7 @@
+import {bootstrap}    from 'angular2/platform/browser'
+import {ClientComponent} from './components/client.component'
+// Add all operators to Observable
+import 'rxjs/Rx';
+
+
+bootstrap(ClientComponent);
